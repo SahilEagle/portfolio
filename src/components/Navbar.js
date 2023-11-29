68e873bb4e1098069a5e5b9ed781080a325ccab4
SahilEagle <skeaglecompany@gmail.com>
2023-11-29 18:38:18 +0530

    everything is preety much done next is to build some project section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,34 @@ import React from 'react';
 // importing fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
             <div className="container-fluid">
                 <a className="navbar-brand text-white" href="/">Sahil</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars} style={{color: "#fafafa",}} />
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#fafafa", }} />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link text-white" aria-current="page" href="#">Home</a>
+                            <a className="nav-link text-white" aria-current="page" href="#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#">Link</a>
+                            <a className="nav-link text-white" href="#About">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#Project">Project</a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link text-white" href="../Sahil_Resume.pdf" download={"SahilResume"}>
+                                Resume <FontAwesomeIcon icon={faDownload} style={{color: "#ffffff",}} />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#Contact">Contact</a>
                         </li>
                     </ul>
                 </div>
