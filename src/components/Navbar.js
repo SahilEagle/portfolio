e7354c058895beb91a12461150bd88d2246cabc4
SahilEagle <skeaglecompany@gmail.com>
2023-12-12 22:03:40 +0530

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React , {useState} from 'react';
 // importing fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const handleToggleClick = () => {
+      setMenuOpen(!isMenuOpen);
+    };
+  
+    const handleMenuItemClick = () => {
+      setMenuOpen(false);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/#">Sahil</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars} style={{ color: "#000", }} />
+                <a className="navbar-brand fs-3" href="/#">Sahil.dev</a>
+                <button
+                    className={`navbar-toggler ${isMenuOpen ? 'collapsed' : ''}`}
+                    type="button"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded={isMenuOpen}
+                    onClick={handleToggleClick}
+                >
+                    <FontAwesomeIcon className='menu' icon={faBars} style={{ color: "#000" }} />
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link text-white" aria-current="page" href="#home">Home</a>
+                            <a className="nav-link text-white" onClick={handleMenuItemClick} aria-current="page" href="#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#About">About</a>
+                            <a className="nav-link text-white" onClick={handleMenuItemClick} href="#About">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#Project">Project</a>
+                            <a className="nav-link text-white" onClick={handleMenuItemClick} href="#Project">Project</a>
                         </li>
                         <li className="nav-item ">
-                            <a className="nav-link text-white" href="../Sahil_Resume.pdf" download={"SahilResume"}>
-                                Resume <FontAwesomeIcon className='resume' icon={faDownload} style={{color: "#000",}} />
+                            <a className="nav-link text-white" onClick={handleMenuItemClick} href="../Sahil_Resume.pdf" download={"SahilResume"}>
+                                Resume <FontAwesomeIcon className='resume' icon={faDownload} style={{ color: "#000", }} />
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#Contact">Contact</a>
+                            <a className="nav-link text-white" onClick={handleMenuItemClick} href="#Contact">Contact</a>
                         </li>
                     </ul>
                 </div>
